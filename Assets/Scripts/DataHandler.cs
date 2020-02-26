d16ff46d01e9b805a3c27af6f3546f0141bf9e12
liamcristello <liamcristello@gmail.com>
2020-02-26 16:56:34 -0500

    Updates to Data Handling, not functional yet

--- Assets/Scripts/DataHandler.cs
+++ Assets/Scripts/DataHandler.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using ReadWriteCSV;
 using UnityEngine;
 
+/**
+ * Writes a line of data to CSV after every trial.
+ */
 public class DataHandler : MonoBehaviour
 {
     // Get the participant ID from Global Control, for actual CSV write purposes
@@ -12,20 +15,39 @@ public class DataHandler : MonoBehaviour
     List<Data> data = new List<Data>();
 
     // Cumulative variables to compare how the user did across all environments
-    private float cumultaiveAccuracyScore;
+    private float cumultaiveAccuracyScore = 0f;
 
     // Data class to store relevant info for each trial. Each field is public readonly.
     class Data
     {
         public readonly string participantID; // ID of participant
         public readonly bool rightHanded; // If participant is rightHanded
+        public readonly GlobalControl.ProgressionType progressionType; // What progression type
+        public readonly GlobalControl.Scene environment; // Which environment they were in
         public readonly float height; // Height from calibration
         public readonly float armLength; // ArmLength from calibration
+        
 
         public readonly int trialNum; // current trial
         public readonly float time; // time the trial took place
 
-        public readonly float accuracyScore; // How many target hits relative to total throws
+        public readonly float accuracyScore; // How many target hits relative to total throws within environment
+        public readonly float cumulativeAccuracyScore; // Accuracy score averaged across all environments
+
+        // Constructor
+        public Data(string participantID, bool rightHanded, float height, float armLength, int trialNum,
+            float time, float accuracyScore, float cumulativeAccuracyScore)
+        {
+            this.participantID = participantID;
+            this.rightHanded = rightHanded;
+            this.height = height;
+            this.armLength = armLength;
+
+            this.trialNum = trialNum;
+            this.time = time;
+            this.accuracyScore = accuracyScore;
+            this.cumulativeAccuracyScore = cumulativeAccuracyScore;
+        }
     }
 
 
